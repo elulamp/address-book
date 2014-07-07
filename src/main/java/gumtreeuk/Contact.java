a69d9d81f8fee5c23ddfb27fa9d3adb297fb254d
Kaur Mätas <kaur.matas@gmail.com>
2014-07-07 22:29:14 +0100

    immutable class that represents contact

---
+++ src/main/java/gumtreeuk/Contact.java
@@ -0,0 +1,41 @@
+package gumtreeuk;
+
+import java.time.LocalDate;
+
+public class Contact {
+
+    public final String name;
+    public final Gender gender;
+    public final LocalDate birthday;
+
+    public Contact(String name, Gender gender, LocalDate birthday) {
+        this.name = name;
+        this.gender = gender;
+        this.birthday = birthday;
+    }
+
+    public static class Builder {
+        private String name = "John Doe";
+        private Gender gender = Gender.MALE;
+        private LocalDate birthday = LocalDate.now();
+
+        public Builder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder withGender(Gender gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        public Builder withBirthday(LocalDate birthday) {
+            this.birthday = birthday;
+            return this;
+        }
+
+        public Contact build() {
+            return new Contact(name, gender, birthday);
+        }
+    }
+}
